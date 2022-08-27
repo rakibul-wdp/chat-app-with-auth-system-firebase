39f731717e9fa6427ec8ee29e38a9f9c4c6c54a6
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-27 21:13:57 +0600

    create chattroom component

---
+++ src/components/Chat/ChatRoom.js
@@ -0,0 +1,52 @@
+import React, { useRef, useState } from 'react';
+import auth from '../../firebase.init';
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firestore = firebase.firestore();
+
+const ChatRoom = () => {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <main>
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder='say something nice' />
+
+        <button type='submit' disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default ChatRoom;
--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+  const date = new Date().getFullYear();
   return (
-    <footer className='footer footer-center p-4 bg-base-300 text-base-content'>
+    <footer className='footer footer-center p-4 bg-base-300 text-base-content absolute bottom-0'>
       <div>
-        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © {date} - All right reserved by ACME Industries Ltd</p>
       </div>
     </footer>
   );
