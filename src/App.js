ccb7cf153ddc637b040aaafc4753087e2bd3d2ec
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-27 19:27:34 +0600

    add login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Navbar from './pages/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
     </div>
   );
 }
