fa27a961c9b9ada2aefc56326ff69e4d7e780647
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-27 20:10:55 +0600

    require auth and rearange navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/SignUp';
-import Navbar from './components/Navbar';
+import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route path='signup' element={<SignUp />} />
       </Routes>
     </div>
