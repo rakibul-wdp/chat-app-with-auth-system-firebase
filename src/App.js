936028fa7fd97b760f09b59038ce4e014e667d82
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-27 20:16:19 +0600

    add simple footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/SignUp';
+import Footer from './components/Shared/Footer';
 import Navbar from './components/Shared/Navbar';
 
 function App() {
@@ -8,9 +9,10 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='login' element={<Login />} />
         <Route path='signup' element={<SignUp />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
